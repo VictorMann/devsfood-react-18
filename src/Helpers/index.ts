8c98a6288d8fa74496a35f759e9fd7388210ad9a
Victor Mann <victorcmann@gmail.com>
2023-04-23 16:53:57 -0300

    initial helpers

---
+++ src/Helpers/index.ts
@@ -0,0 +1,28 @@
+import { ErrorType } from '../Types';
+
+/**
+ * Retorna a classe do dado passado: String, Number, Object
+ * 
+ * @param data {any}
+ * @returns {string}
+ */
+export const classOf = (data: any) => {
+	if (data === null) return "Null";
+	if (data === undefined) return "Undefined";
+	return Object.prototype.toString.call(data).slice(8,-1);
+};
+
+/**
+ * Reposta de erro, verifica se o data da response é um objeto, caso não, retorna statusText 
+ * 
+ * @param response {AxiosError}
+ * @returns {object}
+ */
+export const trataErrorResponse = (err: any): ErrorType => {
+	if (err.response) {
+		if (classOf(err.response?.data) === 'Object') return err.response.data;
+		return {error: err.response?.statusText};
+	}
+
+	return {error: `${err.message}\n${err.config?.baseURL}${err.config?.url}`};
+};
