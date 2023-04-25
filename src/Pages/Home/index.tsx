939b12a9df3b17884f806944049e9cb89b2adb1b
Victor Mann <victorcmann@gmail.com>
2023-04-24 21:37:19 -0300

    Initial Home page

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,14 +1,57 @@
 import * as C from './styles';
-
+import { Link, useSearchParams } from 'react-router-dom';
 import Search from '../../Components/HomeSearch';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { api } from '../../Api';
+import { CategoryType, ErrorType } from '../../Types';
 
 function Page() {
+  const [searchParams, setSearchParams] = useSearchParams();
   const [search, setSearch] = useState<string>('');
+  const [categories, setCategories] = useState<CategoryType[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const resp = await api.categories();
+      if (resp.error) alert(resp.error);
+      else setCategories(resp);
+    })()
+  }, []);
 
   return (
     <C.Container>
       <Search search={search} setSearch={setSearch} />
+
+      <div className='mt-5 text-white'>
+        <p>Selecione uma categoria: </p>
+        <ul className='list-cat list-unstyled'>
+
+          {categories.length > 0 &&
+            <>
+              <li>
+                <Link 
+                  to="/"
+                  className={Number(searchParams.get('cat')) === 0 ? 'cat-all active' : 'cat-all'}>
+                    Todos
+                </Link>
+              </li>
+              {categories.map(item => (
+              <li key={item.id}>
+                <Link 
+                  className={Number(searchParams.get('cat')) === item.id ? 'active' : ''}
+                  to={`?cat=${item.id}`} 
+                  style={{ backgroundImage: `url(${item.image})` }}>
+                  
+                    {item.name}
+
+                </Link>
+              </li>
+              ))}
+            </>
+          }
+        </ul>
+      </div>
+
     </C.Container>
   )
 }
--- src/Pages/Home/styles.ts
+++ src/Pages/Home/styles.ts
@@ -1,3 +1,38 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+
+.list-cat {
+  display: flex;
+
+  a {
+    display: inline-block;
+    width: 70px;
+    height: 70px;
+    font-size: 0;
+
+    background-color: var(--app-border-cart);
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: contain;
+
+    margin-right: .5rem;
+    overflow: hidden;
+    border-radius: 5px;
+
+    &.cat-all {
+      font-size: 1rem;
+      color: white;
+      text-decoration: none;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    &.active {
+      border: 2px solid var(--app-secondary-color);
+    }
+
+  }
+}
+`;
