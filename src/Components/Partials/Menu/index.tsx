a1e4582cc8f417a9764f607c466b0c3fa6afb765
Victor Mann <victorcmann@gmail.com>
2023-04-24 23:53:58 -0300

    added tootip in menu

--- src/Components/Partials/Menu/index.tsx
+++ src/Components/Partials/Menu/index.tsx
@@ -1,6 +1,8 @@
 import * as C from './styles';
 import { Link, useLocation } from 'react-router-dom';
 
+import { Tooltip } from '../../Tooltip';
+
 function Comp() {
   const loc = useLocation();
 
@@ -8,19 +10,25 @@ function Comp() {
     <C.Container className='nav-main d-flex align-items-center'>
       <ul className='list-unstyled'>
         <li>
-          <Link to="/" className={loc.pathname === '/' ? 'active' : ''}>
-            <img src="/images/store.png" alt="" />
-          </Link>
+          <Tooltip title='Loja'>
+            <Link to="/" className={loc.pathname === '/' ? 'active' : ''}>
+              <img src="/images/store.png" alt="" />
+            </Link>
+          </Tooltip>
         </li>
         <li>
-          <Link to="/order" className={loc.pathname === '/order' ? 'active' : ''}>
-            <img src="/images/order.png" alt="" />
-          </Link>
+          <Tooltip title='Pedidos'>
+            <Link to="/order" className={loc.pathname === '/order' ? 'active' : ''}>
+              <img src="/images/order.png" alt="" />
+            </Link>
+          </Tooltip>
         </li>
         <li>
-          <Link to="/profile" className={loc.pathname === '/profile' ? 'active' : ''}>
-            <img src="/images/profile.png" alt="" />
-          </Link>
+          <Tooltip title='Minha Conta'>
+            <Link to="/profile" className={loc.pathname === '/profile' ? 'active' : ''}>
+              <img src="/images/profile.png" alt="" />
+            </Link>
+          </Tooltip>
         </li>
       </ul>
     </C.Container>
