6b94772c89bd7acc1cec4121fea7871c828847b9
Victor Mann <victorcmann@gmail.com>
2023-04-26 14:20:58 -0300

    fix in Category component URL

--- src/Components/CategoryItem/index.tsx
+++ src/Components/CategoryItem/index.tsx
@@ -1,5 +1,5 @@
 import * as C from './styles';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import { CategoryType } from '../../Types';
 
 import { Tooltip } from '../Tooltip';
@@ -10,13 +10,20 @@ type Props = {
 };
 
 function Comp({ item, active }: Props) {
+  const [searchParams, setSearchParams] = useSearchParams();
   let strClass = item.id === 0 ? 'cat-all ' : '';
+
+  const makeURL = (id: number) => {
+    if (id === 0) searchParams.delete('cat');
+    else searchParams.set('cat', String(id));
+    return '?' + searchParams.toString();
+  };
 
   return (
     <C.Container>
       <Tooltip title={item.name as string} dir='top'>
         <Link 
-          to={item.id === 0 ? '/' : `?cat=${item.id}`}
+          to={makeURL(Number(item.id))}
           className={item.id === active ? (strClass + 'active') : strClass}
           style={{ backgroundImage: `url(${item.image})` }}>
 
