c71209fb0dfff20ed188a850a97d12483d2843ba
Victor Mann <victorcmann@gmail.com>
2023-04-27 20:17:10 -0300

    added formatMoeda helper function

--- src/Helpers/index.ts
+++ src/Helpers/index.ts
@@ -46,3 +46,13 @@ export const get = (data: any, type: string|null = null, defalut: any = '') => {
 	}
 	return data;
 };
+
+/**
+ * Converte valor número em moeda
+ * 
+ * @param number {string|number} número a ser convertido
+ * @returns {number}
+ */
+export const formatMoeda = (number: string|number) => {
+	return Number(number).toFixed(2).replaceAll('.', ',');
+};
