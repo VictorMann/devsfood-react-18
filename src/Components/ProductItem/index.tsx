b599a12289babbc5791910ebbf29e3a0e6eb2820
Victor Mann <victorcmann@gmail.com>
2023-04-27 20:17:48 -0300

    added click event in ProductItem Component

--- src/Components/ProductItem/index.tsx
+++ src/Components/ProductItem/index.tsx
@@ -1,20 +1,22 @@
 import * as C from './styles';
 
 import { ProductType } from '../../Types';
+import { formatMoeda } from '../../Helpers';
 
 type Props = {
   item: ProductType;
+  onClick?: (item: ProductType) => void;
 };
 
-function Comp({ item }: Props) {
+function Comp({ item, onClick }: Props) {
   return (
-    <C.Container className='d-flex rounded p-2'>
+    <C.Container className='d-flex rounded p-2' onClick={() => onClick && onClick(item)}>
       <figure>
         <img src={item.image} alt="" />
       </figure>
       <div className='figure-desc'>
         <span className='fig-name'>{item.name}</span>
-        <span className='fig-price'>R$ {item.price}</span>
+        <span className='fig-price'>R$ {formatMoeda(Number(item.price))}</span>
         <span className='fig-ing'>{item.ingredients}</span>
       </div>
     </C.Container>
