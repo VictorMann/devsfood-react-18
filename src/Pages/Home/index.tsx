89d3fbb8ca5a40e87e97deece10074a84561566a
Victor Mann <victorcmann@gmail.com>
2023-04-28 14:08:13 -0300

    new features on home page

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,14 +1,26 @@
 import * as C from './styles';
-import { Link, useSearchParams } from 'react-router-dom';
-import Search from '../../Components/HomeSearch';
 import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { setOpen, setProduct } from '../../Redux/Reducers/ProductModal';
+import Search from '../../Components/HomeSearch';
 import { api } from '../../Api';
-import { CategoryType, ErrorType } from '../../Types';
+import { CategoryType, ProductType } from '../../Types';
+import CategoryItem from '../../Components/CategoryItem';
+import ProductItem from '../../Components/ProductItem';
+
+import { get } from '../../Helpers';
+import { useAppSelector } from '../../Redux/hooks/useAppSelector';
+
+let flagTimer: any = 0;
 
 function Page() {
+  const dispatch = useDispatch();
+  const modal = useAppSelector(state => ({ before: state.ProductModal.stateBefore, after: state.ProductModal.stateAfter }));
   const [searchParams, setSearchParams] = useSearchParams();
-  const [search, setSearch] = useState<string>('');
+  const [search, setSearch] = useState<string>(get(searchParams.get('q')));
   const [categories, setCategories] = useState<CategoryType[]>([]);
+  const [products, setProducts] = useState<ProductType[]>([]);
 
   useEffect(() => {
     (async () => {
@@ -18,39 +30,72 @@ function Page() {
     })()
   }, []);
 
+  useEffect(() => { searchData() }, [searchParams.get('cat'), searchParams.get('q')]);
+
+  useEffect(() => {
+    if (search === '' || search.length > 2) {
+      clearTimeout(flagTimer);
+      flagTimer = setTimeout(() => {
+        if (!search) searchParams.delete('q');
+        else searchParams.set('q', search);
+        setSearchParams(searchParams);
+      }, 1000);
+    }
+  }, [search]);
+
+  async function searchData() {
+    let cat = searchParams.get('cat') ? Number(searchParams.get('cat')) : '';
+    const resp = await api.products({ cat, q: search });
+    if (resp.error) { alert(resp.error); setProducts([]);}
+    else setProducts(resp);
+  }
+
+  const handleClickProduct = (item: ProductType) => {
+    let fn = function() {
+      var el: any = document.querySelector('.nav-main');
+      if (el) el.style.backgroundColor = 'red';
+    };
+
+    dispatch( setProduct(item) );
+    dispatch( setOpen(true) );
+  };
+
+
   return (
     <C.Container>
       <Search search={search} setSearch={setSearch} />
 
       <div className='mt-5 text-white'>
-        <p>Selecione uma categoria: </p>
-        <ul className='list-cat list-unstyled'>
+        <p>Selecione uma categoria: Before: {modal.before ? 'SIM' : 'NAO'} After: {modal.after ? 'SIM' : 'NAO'}</p>
+        <ul className='list-cat list-unstyled mb-2'>
 
           {categories.length > 0 &&
             <>
-              <li>
-                <Link 
-                  to="/"
-                  className={Number(searchParams.get('cat')) === 0 ? 'cat-all active' : 'cat-all'}>
-                    Todos
-                </Link>
-              </li>
+              <CategoryItem 
+                item={{id: 0, name: 'Todos', image: ''}} 
+                active={Number(searchParams.get('cat'))} />
+                
               {categories.map(item => (
-              <li key={item.id}>
-                <Link 
-                  className={Number(searchParams.get('cat')) === item.id ? 'active' : ''}
-                  to={`?cat=${item.id}`} 
-                  style={{ backgroundImage: `url(${item.image})` }}>
-                  
-                    {item.name}
-
-                </Link>
-              </li>
+                <CategoryItem 
+                  key={item.id} 
+                  item={item} 
+                  active={Number(searchParams.get('cat'))} />
               ))}
             </>
           }
         </ul>
       </div>
+
+      <ul className='list-products list-unstyled'>
+        {products.length > 0 &&
+          products.map(item => (
+            <ProductItem 
+              key={item.id} 
+              item={item}
+              onClick={handleClickProduct} />
+          ))
+        }
+      </ul>
 
     </C.Container>
   )
--- src/Pages/Home/styles.ts
+++ src/Pages/Home/styles.ts
@@ -4,35 +4,11 @@ export const Container = styled.div`
 
 .list-cat {
   display: flex;
+}
 
-  a {
-    display: inline-block;
-    width: 70px;
-    height: 70px;
-    font-size: 0;
-
-    background-color: var(--app-border-cart);
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: contain;
-
-    margin-right: .5rem;
-    overflow: hidden;
-    border-radius: 5px;
-
-    &.cat-all {
-      font-size: 1rem;
-      color: white;
-      text-decoration: none;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
-
-    &.active {
-      border: 2px solid var(--app-secondary-color);
-    }
-
-  }
+.list-products {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 20px;
 }
 `;
