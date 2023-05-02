df0599eb706c2b628d717da58845c9394e795c9a
Victor Mann <victorcmann@gmail.com>
2023-05-02 15:00:19 -0300

    enter price on products

--- db/devsfood.sql
+++ db/devsfood.sql
@@ -21,10 +21,10 @@ CREATE TABLE product (
   image VARCHAR(1024) NOT NULL
 );
 
-INSERT INTO product (id, category_id, name, ingredients, image) VALUES
-(1, 1, 'Torta de chocolate', '500g de farinha, 1 col. de sopa de chocolate, 3x ovo', 'http://localhost:3001/images/torta1.jpg'),
-(2, 1, 'Torta de limão', '350g de farinha, 350ml de suco de limão', 'http://localhost:3001/images/torta2.jpg'),
-(3, 2, 'Donut de moranga', 'farinho, ovo, etc.', 'http://localhost:3001/images/donut1.jpg'),
-(4, 3, 'Cookie de gotas de chocolate', 'leite em pó, farinha e chocolate', 'http://localhost:3001/images/cookie1.jpg');
+INSERT INTO product (id, category_id, name, price, ingredients, image) VALUES
+(1, 1, 'Torta de chocolate', 3.5, '500g de farinha, 1 col. de sopa de chocolate, 3x ovo', 'http://localhost:3001/images/torta1.jpg'),
+(2, 1, 'Torta de limão', 7.35, '350g de farinha, 350ml de suco de limão', 'http://localhost:3001/images/torta2.jpg'),
+(3, 2, 'Donut de moranga', 1.99, 'farinho, ovo, etc.', 'http://localhost:3001/images/donut1.jpg'),
+(4, 3, 'Cookie de gotas de chocolate', 2.4, 'leite em pó, farinha e chocolate', 'http://localhost:3001/images/cookie1.jpg');
 
 COMMIT;
