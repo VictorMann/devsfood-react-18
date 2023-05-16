790fa6676e44a89ffd810d7dadad2cce04ef2d6e
Victor Mann <victorcmann@gmail.com>
2023-05-16 19:58:17 -0300

    adjustement Modal

--- src/Components/Modal/index.tsx
+++ src/Components/Modal/index.tsx
@@ -1,7 +1,6 @@
 import * as C from './styles';
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { setOpen } from '../../Redux/Reducers/ProductModal';
 
 import { delay } from '../../Helpers';
 
@@ -9,9 +8,11 @@ type Props = {
   children: JSX.Element;
   onBeforeProcess?: (state: boolean) => any;
   onAfterProcess?: (state: boolean) => any;
+  setClose: (...key: any[]) => any;
+  styles?: {[key: string]: any};
 }
 
-function Comp({ children, onBeforeProcess, onAfterProcess }: Props) {
+function Comp({ children, onBeforeProcess, onAfterProcess, setClose, styles = {} }: Props) {
   const dispatch = useDispatch();
   const [active, setActive] = useState<boolean>(false);
 
@@ -26,14 +27,14 @@ function Comp({ children, onBeforeProcess, onAfterProcess }: Props) {
     if (!e.target.classList.contains('close-modal')) return;
     setActive(false);
     await delay(300);
-    dispatch( setOpen(false) );
+    setClose();
     onBeforeProcess && dispatch( onBeforeProcess(false) );
     onAfterProcess && dispatch( onAfterProcess(false) );
   };
 
   return (
     <C.Container className='close-modal' onClick={fnClose}>
-      <div className={active ? 'modal-body active' : 'modal-body'}>
+      <div className={active ? 'modal-body active' : 'modal-body'} style={{...styles}}>
         {children}
       </div>
     </C.Container>
