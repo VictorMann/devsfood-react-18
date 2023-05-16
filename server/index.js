82a552af9556d386eed0df1d1742f2c67195d6b6
Victor Mann <victorcmann@gmail.com>
2023-05-16 19:56:12 -0300

    inserted routes in api and server

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3');
 const fs = require('fs');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const PORT = process.env.PORT;
 
@@ -19,7 +21,81 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
+// middleware para verificar o token JWT
+const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization;
 
+  if (!token) {
+    return res.status(401).json({ error: 'Token não fornecido' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.email = decoded.email;
+    next();
+  } catch (err) {
+    res.status(401).json({ error: 'Token inválido' });
+  }
+};
+
+// Register
+app.post('/register', (req, res) => {
+  const { user, end } = req.body;
+  db.get('SELECT MAX(id) as id FROM user', (err, result) => {
+    if (err) throw err;
+    let user_id = 1;
+    if (result.id) user_id = result.id + 1;
+    const password = bcrypt.hashSync(user.password, 10);
+    db.run('INSERT INTO user VALUES (?, ?, ?, ?)', [user_id, user.name, user.email, password], (err) => {
+      if (err) throw err;
+      console.log(`Usuário: ${user.email} - criado com sucesso!`);
+
+      db.get('SELECT MAX(ID) as id FROM endereco', (err, result) => {
+        if (err) throw err;
+        let end_id = 1;
+        if (result.id) end_id = result.id + 1;
+        db.run('INSERT INTO endereco VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', [end_id, user_id, end.endereco, end.numero, end.complemento, end.cep, end.cidade, end.bairro, end.uf], (err) => {
+          if (err) throw err;
+          console.log(`Endereço: ${end.endereco} - criado com sucesso!`);
+  
+          // Gerar um token JWT
+          const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET, { expiresIn: '6h' });
+          // Retornar o token JWT
+          return res.json({ token });
+        });
+      });
+
+    });
+  });
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  db.get('SELECT * FROM user WHERE email = ? LIMIT 1', email, (err, result) => {
+    if (err) throw err;
+    // verifica se encontrou o usuário do email
+    if (!result) return res.status(404).json({ error: 'e-mail não encontrado' });
+    // Verificar as informações do usuário
+    if (!bcrypt.compareSync(password, result.password)) {
+      return res.status(404).json({ error: 'Usuário ou senha incorretos' });
+    }
+    // Gerar um token JWT
+    const token = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: '6h' });
+    // Retornar o token JWT
+    return res.json({ token });
+  });
+});
+
+app.get('/user', verifyToken, (req, res) => {
+  db.get('SELECT * FROM user WHERE email = ?', req.email, (err, result) => {
+    if (err) throw err;
+    const user = result;
+    db.get('SELECT * FROM endereco WHERE user_id = ?', user.id, (err, result) => {
+      if (err) throw err;
+      return res.json({ user, end: result});
+    });
+  });
+});
 
 // Rota teste
 app.get('/categories', (req, res) => {
@@ -56,6 +132,20 @@ app.get('/products', (req, res) => {
   }); 
 });
 
+// Editar Endereço
+app.put('/endereco', verifyToken, (req, res) => {
+  let end = req.body;
+  let sql = `UPDATE endereco 
+  SET endereco = ?, numero = ?, complemento = ?,
+  cep = ?, cidade = ?, bairro = ?, uf = ? 
+  WHERE id = ?`;
+  let vals = [end.endereco, end.numero, end.complemento, end.cep, end.cidade, end.bairro, end.uf, end.id];
+  db.run(sql, vals, (err) => {
+    if (err) throw err;
+    return res.json(req.body);
+  });
+});
+
 
 // Rota de Imagem
 app.get('/images/:file', (req, res) => {
@@ -66,6 +156,7 @@ app.get('/images/:file', (req, res) => {
     else res.sendFile(filepath);
   });
 });
+
 
 
 app.listen(PORT, () => {
--- src/Api.ts
+++ src/Api.ts
@@ -1,12 +1,66 @@
 import axios from "axios";
 import { CategoryType, ErrorType, ProductType } from './Types';
 import { trataErrorResponse } from "./Helpers";
+import { UserType, EnderecoType } from './Types';
+import { getToken } from "./Helpers/auth";  
 
 const http = axios.create({
   baseURL: 'http://localhost:3001'
 });
 
+const wsViaCEP = 'https://viacep.com.br/ws/{{CEP}}/json/'; // https://viacep.com.br/
+
 export const api = {
+  
+  /**
+   * dados do usuário logado
+   * @returns 
+   */
+  async user() {
+    let data: UserType | ErrorType | any;
+    try {
+      const resp = await http.get('/user', { headers: { Authorization: getToken() } } );
+      data = resp.data;
+    } catch (e: any) {
+      data = trataErrorResponse(e);
+    }
+
+    return data;
+  },
+
+  /**
+   * Login
+   * @param email 
+   * @param password 
+   * @returns 
+   */
+  async login(email: string, password: string) {
+    let data: any;
+    try {
+      const resp = await http.post('/login', { email, password });
+      data = resp.data;
+    } catch (e: any) {
+      data = trataErrorResponse(e);
+    }
+    return data;
+  },
+
+  /**
+   * Cadastro de Usuário
+   * @param user 
+   * @param end 
+   * @returns 
+   */
+  async register(user: UserType, end: EnderecoType) {
+    let data: any;
+    try {
+      const resp = await http.post('/register', { user, end });
+      data = resp.data;
+    } catch(e: any) {
+      data = trataErrorResponse(e);
+    }
+    return data;
+  },
 
   /**
    * Retorna a lista de Categorias
@@ -25,20 +79,51 @@ export const api = {
     return data;  
   },
 
-    /**
+  /**
    * Retorna a lista de Categorias
    * 
    * @returns 
    */
-    async products(filters: Object) {
-      let data: ProductType[] | ErrorType | any;
-      try {
-        const resp = await http.get('/products', { params: filters });
-        data = resp.data;
-      } catch (e: any) {
-        data = trataErrorResponse(e);
-      }
+  async products(filters: Object) {
+    let data: ProductType[] | ErrorType | any;
+    try {
+      const resp = await http.get('/products', { params: filters });
+      data = resp.data;
+    } catch (e: any) {
+      data = trataErrorResponse(e);
+    }
+
+    return data;  
+  },
+
+  /**
+   * Consulta CEP via Web Service (Serviço: https://viacep.com.br/)
+   * @param cep {string}
+   * @returns 
+   */
+  async getCEP(cep: string = '02041040') {
+    let data: ErrorType | any;
+    try {
+      let xCep = cep.replace(/\D/g, ''); // somente número
+      let url = wsViaCEP.replace('{{CEP}}', xCep);
+      const resp = await axios.get(url);
+      data = resp.data;
+    } catch (e: any) {
+      data = trataErrorResponse(e);
+    }
+    return data;
+  },
+
+  async editEnd(end: EnderecoType) {
+    let data: ErrorType | any;
+    try {
+      const resp = await http.put('/endereco', end, { headers: { Authorization: getToken() } });
+      data = resp.data;
+    } catch (e: any) {
+      data = trataErrorResponse(e);
+    }
+    return data;
+  }
   
-      return data;  
-    },
 };
+
