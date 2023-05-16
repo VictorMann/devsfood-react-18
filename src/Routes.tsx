4b2bafb8c4c62c769050fdbbfc14fe4ee69fa8fc
Victor Mann <victorcmann@gmail.com>
2023-05-16 20:03:53 -0300

    add /profile route

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './Pages/Home';
+import Profile from './Pages/Profile';
 import NotFound from './Pages/NotFound';
 
 export default () => {
   return (
     <Routes>
       <Route path='/' element={ <Home /> } />
+      <Route path='/profile' element={ <Profile /> } />
       <Route path='*' element={ <NotFound /> } />
     </Routes>
   )
